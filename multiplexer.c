393f8fe71be29e5f8d541846ad74fd0027e893f7
SamT <example@example.com>
2020-01-24 16:45:35 +0000

    added four one multiplexing

--- multiplexer.c
+++ multiplexer.c
@@ -11,7 +11,7 @@ int multiplexer(int A, int B, int SL){
 }
 
 ByteBlock multiplexer_eight_bit(const ByteBlock A, const ByteBlock B, int SL){
-    if (SL == 0){
+    /*if (SL == 0){
         return A;
     }
     else if (SL == 1){
@@ -21,14 +21,19 @@ ByteBlock multiplexer_eight_bit(const ByteBlock A, const ByteBlock B, int SL){
     {
         printf("dumbass\n");
         return EMPTY_BYTE_BLOCK();
-    }
+    }*/
     
-    /*ByteBlock Z = EMPTY_BYTE_BLOCK();
+    ByteBlock Z = EMPTY_BYTE_BLOCK();
     int bit;
     for (bit = 0; bit < 8; bit += 1){
         Z.block[bit] = multiplexer(A.block[bit], B.block[bit], SL);
     }
-    return Z;*/
+    return Z;
+}
+
+ByteBlock four_input_multiplexer(ByteBlock A, ByteBlock B, ByteBlock C, ByteBlock D, int SL0, int SL1){
+    ByteBlock result = multiplexer_eight_bit(multiplexer_eight_bit(A, B, SL0), multiplexer_eight_bit(C, D, SL0), SL1);
+    return result;
 }
 
 int main(){
